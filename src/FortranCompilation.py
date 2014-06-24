76d9638b61fec2616efe13ac35cc109a3d56e4f2
Bruce Ravel <bravel@bnl.gov>
2014-06-24 17:55:13 -0400

    some comments in FortranCompilation.py

--- src/FortranCompilation.py
+++ src/FortranCompilation.py
@@ -1,12 +1,17 @@
 
 from SCons.Environment import Environment
-#from SCons.Script import *
 
 def CompilationEnvironment():
     env = Environment()
 
     if env['FORTRAN'] == 'gfortran':
-        env = Environment(FORTRANFLAGS = '-O3 -ffree-line-length-none')   ## -Wall -finit-local-zero
+        # this was the suggestion in the top level Makefile in what
+        # the FP gave us: "-O3 -ffree-line-length-none -finit-local-zero"
+        # -O3 makes sense, as does -finit-local-zero.  I don't understand
+        # the advantage of -ffree-line-length-none -- it seems to 
+        # encourage poor code style -- like we need more of that!
+        # also,  -finit-local-zero fails on FMS/fmstot.f
+        env = Environment(FORTRANFLAGS = '-O3 -ffree-line-length-none -Wall')   ## -Wall -finit-local-zero
     elif env['FORTRAN'] == 'g77':
         env = Environment(FORTRANFLAGS = '-Wall -O2')
     elif env['FORTRAN'] == 'xlf':
