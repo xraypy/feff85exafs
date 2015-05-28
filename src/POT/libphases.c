99c290fde6bb43cd0d9f10f1a1c9f8486130e6b3
Bruce Ravel <bravel@bnl.gov>
2015-05-28 15:06:07 -0400

    begin work on C interface to pot+xsph

--- src/POT/libphases.c
+++ src/POT/libphases.c
@@ -29,6 +29,8 @@ _EXPORT(long) create_phases(FEFFPHASES *phases) {
   /* Return an error code -- currently hardwired to return 0. */
   long i;
 
+  char titl[81] = {'\0'};
+  char ptlb[7]  = {'\0'};
 
   phases->ntitle = 0;
   phases->nat    = 0;
@@ -57,9 +59,9 @@ _EXPORT(long) create_phases(FEFFPHASES *phases) {
   /* allocate string arrays for titles and potlbl */
 
 
-  path->rat  = calloc(natx, sizeof(double *));
+  phases->rat  = calloc(natx, sizeof(double *));
   for (i = 0; i < natx; i++) {
-    path->rat[i] = calloc(3, sizeof(double));
+    phases->rat[i] = calloc(3, sizeof(double));
   }
 
   phases->iz     = calloc(nphx+1, sizeof(long));
@@ -76,17 +78,25 @@ _EXPORT(long) create_phases(FEFFPHASES *phases) {
   phases->xion   = calloc(nphx+1, sizeof(double));
 
 
-  path->ptz  = calloc(3, sizeof(double *));
+  phases->ptz  = calloc(3, sizeof(double *));
   for (i = 0; i < 3; i++) {
-    path->ptz[i] = calloc(3, sizeof(double));
+    phases->ptz[i] = calloc(3, sizeof(double));
   }
 
+  phases->titles = malloc(sizeof(char *) *  nheadx * 7);
+  for (i = 0; i < nheadx; i++) {
+    strcpy(phases->titles[i], titl);
+  }
+  phases->potlbl = malloc(sizeof(char *) * (nphx+1) * 81);
+  for (i = 0; i < nphx+1; i++) {
+    strcpy(phases->potlbl[i], ptlb);
+  }
 
   return 0;
 }
 
 
-_EXPORT(void) clear_path(FEFFPHASES *phases) {
+_EXPORT(void) clear_phases(FEFFPHASES *phases) {
   /* Reinitialize a FEFFPHASES struct, returning everything to default */
   long i,j;
 
@@ -154,17 +164,23 @@ _EXPORT(void) clear_path(FEFFPHASES *phases) {
 _EXPORT(long) make_phases(FEFFPHASES *phases) {
   /* Instantiate and initialize a FEFFPHASES struct */
   /* Return an error code -- currently hardwired to return 0. */
-  long i;
+  long i, j;
 
   long ntitle, nat, nph, ihole, lscf, nscmt, nmix, icoul, ipol, ispin, iafolp, iunf, inters, jumprm, nohole;
   double rscf, ca, ecv, elpty, angks, gamach, rgrd, totvol;
-  long iz[nphx+1], lmaxsc[nphx+1], lmaxph[nphx+1];
+  long iz[nphx+1], lmaxsc[nphx+1], lmaxph[nphx+1], iphat[natx];
   double xnatph[nphx+1], spinph[nphx+1], folp[nphx+1], xion[nphx+1];
   double rat[natx][3], ptz[3][3];
   double evec[3], xivec[3], spvec[3];
+  char potlbl[nphx+1][7];
+  char titles[nheadx][81];
 
 
-  for (i = 0; i < nphx; i++) {
+  for (i = 0; i < ntitle; i++) {
+    strcpy(titles[i], phases->titles[i]);
+  };
+
+  for (i = 0; i < nph; i++) {
     iz[i]     = phases->iz[i];
     lmaxsc[i] = phases->lmaxsc[i];
     lmaxph[i] = phases->lmaxph[i];
@@ -175,7 +191,7 @@ _EXPORT(long) make_phases(FEFFPHASES *phases) {
     strcpy(potlbl[i], phases->potlbl[i]);
   };
 
-  for (i = 0; i < natx; i++) {
+  for (i = 0; i < nat; i++) {
     for (j = 0; j < 3; j++) {
       rat[i][j] = phases->rat[i][j];
     }
@@ -200,7 +216,41 @@ _EXPORT(long) make_phases(FEFFPHASES *phases) {
 	    &ihole, &rscf, &lscf, &nscmt, &ca, &nmix, &ecv, &icoul,
 	    &ipol, &evec, &elpty, &xivec, &ispin, &spvec, &angks,
 	    &ptz, &gamach, 
-	    &iafolp, &folp, &xion, &rgrd, &iunf, &inters, &voltot, &jumprm, &nohole );
+	    &iafolp, &folp, &xion, &rgrd, &iunf, &inters, &totvol, &jumprm, &nohole );
 
   return 0;
 }
+
+
+_EXPORT(void) cleanup(FEFFPHASES *phases) {
+
+  long i;
+
+  for (i = 0; i < natx; i++) {
+    free(phases->rat[i]);
+  }
+  free(phases->rat);
+
+  free(phases->iz);
+  free(phases->lmaxsc);
+  free(phases->lmaxph);
+  free(phases->xnatph);
+  free(phases->spinph);
+
+  free(phases->evec);
+  free(phases->xivec);
+  free(phases->spvec);
+
+  free(phases->folp);
+  free(phases->xion);
+
+
+  for (i = 0; i < 3; i++) {
+    free(phases->ptz[i]);
+  }
+  free(phases->ptz);
+
+  free(phases->titles);
+  free(phases->potlbl);
+
+};
