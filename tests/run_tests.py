ae1c36c0c63dde89bc8fa20be7fb32d063379b2a
Matthew Newville <newville@cars.uchicago.edu>
2017-04-14 16:34:50 -0500

    update run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-from os.path import isfile, isdir, join
+from os.path import isfile, isdir, join, exists
 import re
 
 from math import isnan
@@ -11,217 +11,129 @@ from math import isnan
 import nose
 import pytest
 
-
 import larch
 from larch_plugins.xafs.feffdat import feffpath
 from larch_plugins.io import read_ascii
 
 from f85ut import ut
 
-ALL_FOLDERS = ('Copper', 'NiO', 'UO2', 'Zircon', 'ferrocene', 'bromoadamantane', 'LCO-para', 'LCO-perp')
+ALL_FOLDERS = ('Copper', 'NiO', 'UO2', 'Zircon', 'ferrocene',
+               'bromoadamantane', 'LCO-para', 'LCO-perp')
 
-class Feff8Tests:
-    def __init__(self, folders=None, verbose=True, doplot=False, doscf=None):
-        self.tests = {}
-        if folders is None:
-            folders = ALL_FOLDERS
+class Feff8Test:
+    def __init__(self, folder, verbose=True, doplot=False, doscf=None):
+        if folder not in ALL_FOLDERS:
+            raise ValueError("Must choose one of the available tests:\n  %s\n" % repr(ALL_FOLDERS))
 
         if doscf is None:
-            doscf  =  (os.getenv('FEFF_TEST_SCF', 'False').lower() in ("yes", "true", "on", "y", "t", "1"))
-        for f in folders:
-            this = ut(f)
-            this.verbose = verbose
-            this.doplot = doplot
-            this.doscf = doscf
-            self.tests[f] = this
+            envar = os.getenv('FEFF_TEST_SCF', 'False')
+            doscf = envar.lower() in ("yes", "true", "on", "y", "t", "1")
+
+        self.folder = folder
+        self.test = ut(folder)
+        self.test.verbose = verbose
+        self.test.doplot = doplot
+        self.test.doscf = doscf
 
     def test_feffrun(self):
         "run feff"
-        for name, test in self.tests.items():
-            test.run()
-            scf = 'without SCF'
-            if test.doscf:
-                scf = 'with SCF'
-            with open(join(test.testrun, 'f85e.log'), 'r') as log:
-                # f85e.log shouldn't be more than a couple thousand lines long (ferrocene w/SCF is 1096)
-                lines = log.readlines()
-                m = re.search('Done with module 6:', lines[-1])
-            assert m and test.available(1), "feff run on %s (%s) not successful" % (name, scf)
+        self.test.run()
+        scf = 'without SCF'
+        if self.test.doscf:
+            scf = 'with SCF'
+        # f85e.log shouldn't be more than a couple thousand lines long
+        # (ferrocene w/SCF is 1096)
+        with open(join(self.test.testrun, 'f85e.log'), 'r') as log:
+            lines = log.readlines()
+            m = re.search('Done with module 6:', lines[-1])
+        assert m and self.test.available(1), "feff run on %s (%s) not successful" % (self.folder, scf)
 
 
     def test_feff(self):
         """check F_eff for each path"""
-        for name, test in self.tests.items():
-            if not test.feffran:
-                assert False, "failed to find results of feff calculation for %s" % name
-            for path in test.paths:
-                index = int(path[4:8])
-                for part in ('feff', 'amp', 'phase'):
-                    result = test.compare(index, part=part)
-                    assert result, "comparison of %s for path %d in %s" % (part, index, name)
-
-    def test_columns(self):
-        """check the feffNNNN.dat columns that are the same for all paths"""
-        for f in self.tests:
-            for part in ('lambda', 'caps', 'redfact', 'rep'):
-                check_columns(f, part)
-
-
-    def test_radii(self):
-        for f in self.tests:
-            check_radii(f, 'muffintin')
-            check_radii(f, 'norman')
-
+        assert self.test.feffran, "no test results found for %s" % self.folder
+        for path in self.test.paths:
+            index = int(path[4:8])
+            for part in ('feff', 'amp', 'phase',
+                         'lam', 'phc', 'red_fact', 'rep'):
+                result = self.test.compare(index, part=part)
+                assert result, "comparison of %s for path %d in %s" % (part, index, self.folder)
 
     def test_terms(self):
         "check various quantities computed by feff"
-        for f in self.tests:
+        assert self.test.feffran, "no test results found for %s" % self.folder
+        for npath in range(1, 10):
+            basefile = join(self.test.baseline, 'feff%4.4i.dat' % npath)
+            if not exists(basefile):
+                continue
+            bl = feffpath(join(self.test.baseline, 'feff%4.4i.dat' % npath))
+            tr = feffpath(join(self.test.testrun,  'feff%4.4i.dat' % npath))
             for term in ('edge', 'gam_ch', 'kf', 'mu', 'rs_int', 'vint'):
-                check_feffterms(f, term)
+                tdiff = getattr(bl._feffdat, term) - getattr(tr._feffdat, term)
+                assert abs(tdiff) < 1.e-4, "feff term %s not close enough for %s" % (term, self.folder)
 
-    def test_s02(self):
-        "check that S02 was computed correctly"
-        for f in self.tests:
-            check_s02(f)
+        for radius in ('muffintin', 'norman'):
+            bl = self.test.radii('baseline', radius)
+            tr = self.test.radii('testrun',  radius)
+            assert bl == tr, "list of %s radii are different for %s" % (radius, self.folder)
+        assert self.test.s02() == self.test.s02('baseline'), "s02 calculated incorrectly for %s" % self.folder
 
+    def test_clean(self):
+        """remove the test run"""
+        self.test.clean()
+        assert not isdir(self.test.testrun), "clean up %s calculation not successful" % self.folder
 
-    @pytest.mark.skip(reason="not testing wrapper")
     def test_columns_wrapper(self):
-        check_skip("not checking wrapper")
-        # if not tests[folders[0]].wrapper_available:
-        #     yield check_skip, "wrapper unavailable"
-        # else:
-        #     for f in folders:
-        #         for part in ('lambda', 'caps', 'redfact', 'rep'):
-        #             yield check_columns_wrapper, f, part
+        assert self.test.feffran, "no test results found for %s" % self.folder
+        msg = "comparison of %s for path %i in %s using wrapper"
 
-    @pytest.mark.skip(reason="not testing wrapper")
-    def test_feff_wrapper(self):
-        pass
-        # if not tests[folders[0]].wrapper_available:
-        #     yield check_skip, "wrapper unavailable"
-        # else:
-        #     for f in folders:
-        #         for path in tests[f].paths:
-        #             index = int(path[4:8])
-        #             for part in ('feff', 'amp', 'phase'):
-        #                 if f == ('LCO-perp'):
-        #                     tests[f].sp.evec  = (1,1,0)
-        #                     tests[f].sp.xivec = (0,0,1)
-        #                     tests[f].sp.elpty = 1
-        #                 elif f == ('LCO-para'):
-        #                     tests[f].sp.evec  = (0,0,1)
-        #                 yield check_feff_wrapper, f, index, part
+        for path in self.test.paths:
+            index = int(path[4:8])
+            for part in ('feff', 'amp', 'phase', 'lam', 'phc', 'red_fact', 'rep'):
+                result = self.test.compare(index, part=part, use_wrapper=True)
+                assert result, msg % (part, index, self.folder)
+
+
+    def test_fit(self):
+        """check various fitting and statistical parameters from a canned fit"""
+        assert self.test.feffran, "no test results found for %s" % self.folder
+
+        test_script = join(self.test.path, self.test.folder+'.py')
+        stat_msg = "statistic %s evaluated inconsistently for %s (%.5f %.5f)"
+        param_msg = "%s of fitting parameter %s evaluated inconsistently for %s (%.5f %.5f)"
+
+        if isfile(test_script):
+            self.test.fit()
+            eps = self.test.epsfit
+            for stat in ('chi_reduced', 'chi_square', 'rfactor'):
+                bl = getattr(self.test.blfit.params, stat)
+                tr = getattr(self.test.trfit.params, stat)
+                close = abs((bl-tr)/bl) < eps
+                assert close, stat_msg % (stat, self.folder, bl, tr)
+
+            for par in self.test.blfit.params.covar_vars:
+                for part in ('value', 'stderr'):
+                    bl = getattr(getattr(self.test.blfit.params, par), part)
+                    tr = getattr(getattr(self.test.trfit.params, par), part)
+                    close = abs((bl-tr)/bl) < eps
+                    assert close, param_msg % (part, par, self.folder, bl, tr)
 
 
     @pytest.mark.skip(reason="not testing opconsat")
     def test_feffrun_opconsat(self):
-        check_skip("not checking opconsat")
-        # for f in self.tests:
-        #    check_feffrun_opconsat(f)
+        oca_ok = self.test.run_opconsat()
+        assert oca_ok, "opconsat run on %s not successful" % folder
 
     @pytest.mark.skip(reason="not testing opconsat")
     def test_opconsat(self):
         check_skip("not checking opconstat")
+        self.test.run_opconsat()
         for f in self.tests:
             pass # check_opconsat(f)
-
-    def test_fit(self):
-        """check various fitting and statistical parameters from a canned fit"""
-        for f in self.tests:
-            #if not tests[f].feffran: tests[f].run()
-            if isfile(join(tests[f].path, tests[f].folder+'.skip')):
-                check_skip("skipping data test for %s" % f)
-            elif isfile(join(tests[f].path, tests[f].folder+'.py')):
-                tests[f].fit()
-                for p in ('chi_reduced', 'chi_square', 'rfactor'):
-                    check_stat(f, p)
-                if (hasattr(tests[f].blfit.params, 'covar_vars')):
-                    for p in tests[f].blfit.params.covar_vars:
-                        check_param(f, p, 'value')
-                        check_param(f, p, 'stderr')
-                else:
-                    check_false("fit could not evaluate uncertainties for %s" % f)
-            else:
-                check_skip("no data tests for %s" % f)
-
-    def test_clean(self):
-        """remove the test run"""
-        for f in self.tests:
-            tests[f].clean()
-            check_clean(f)
 
 
 ################################################################################
 
-def check_feffrun(folder):
-    tests[folder].run()
-    scf = 'without SCF'
-    if tests[folder].doscf:
-        scf = 'with SCF'
-    with open(join(tests[folder].testrun,'f85e.log'), 'r') as log:
-        lines = log.readlines() # f85e.log shouldn't be more than a couple thousand lines long (ferrocene w/SCF is 1096)
-        m = re.search('Done with module 6:', lines[-1])
-    assert m and tests[folder].available(1), "feff run on %s (%s) not successful" % (folder, scf)
-
-def check_feffrun_opconsat(folder):
-    oca_ok = tests[folder].run_opconsat()
-    assert oca_ok, "opconsat run on %s not successful" % folder
-
-def check_columns(folder, part):
-    if not tests[folder].feffran: assert False, "failed to find results of feff calculation for %s" % folder
-    this = tests[folder].compare(1, part=part)
-    assert this, "comparison of %s for path 1 in %s" % (part, folder)
-def check_columns_wrapper(folder, part):
-    if not tests[folder].feffran: assert False, "failed to find results of feff calculation for %s" % folder
-    this = tests[folder].compare(1, part=part, use_wrapper=True)
-    assert this, "comparison of %s for path 1 in %s using wrapper" % (part, folder)
-
-def check_feff(folder, index, part):
-    if not tests[folder].feffran: assert False, "failed to find results of feff calculation for %s" % folder
-    this = tests[folder].compare(index, part=part)
-    assert this, "comparison of %s for path %d in %s" % (part, index, folder)
-
-def check_feff_wrapper(folder, index, part):
-    if not tests[folder].feffran: assert False, "failed to find results of feff calculation for %s" % folder
-    this = tests[folder].compare(index, part=part, use_wrapper=True)
-    assert this, "comparison of %s for path %d in %s using wrapper" % (part, index, folder)
-
-def check_radii(folder, radius):
-    if not tests[folder].feffran: assert False, "failed to find results of feff calculation for %s" % folder
-    bl = tests[folder].radii('baseline', radius)
-    tr = tests[folder].radii('testrun',  radius)
-    assert bl == tr, "list of %s radii are different for %s" % (radius, folder)
-
-def check_feffterms(folder, term):
-    if not tests[folder].feffran: assert False, "failed to find results of feff calculation for %s" % folder
-    bl = feffpath(join(tests[folder].baseline, 'feff0001.dat'))
-    tr = feffpath(join(tests[folder].testrun,  'feff0001.dat'))
-    assert abs(getattr(bl._feffdat, term) - getattr(tr._feffdat, term)) < tests[folder].eps4, "feff term %s calculated incorrectly for %s" % (term, folder)
-
-def check_s02(folder):
-    if not tests[folder].feffran: assert False, "failed to find results of feff calculation for %s" % folder
-    assert tests[folder].s02() == tests[folder].s02('baseline'), "s02 calculated incorrectly for %s" % folder
-
-def check_true(msg):
-    assert True, msg
-
-def check_false(msg):
-    assert False, msg
-
-def check_skip(msg):
-    pass
-
-def check_param(folder, param, part):
-    bl=getattr(getattr(tests[folder].blfit.params, param), part)
-    tr=getattr(getattr(tests[folder].trfit.params, param), part)
-    assert abs((bl-tr)/bl) < tests[folder].epsfit, "%s of fitting parameter %s evaluated inconsistently for %s (%.5f %.5f)" % (part, param, folder, bl, tr)
-
-## this tests for < 0.1% deviation in statistic value, scaled to the size of the statistic
-def check_stat(folder, param):
-    bl=getattr(tests[folder].blfit.params, param)
-    tr=getattr(tests[folder].trfit.params, param)
-    assert abs((bl-tr)/bl) < tests[folder].epsfit, "statistic %s evaluated inconsistently for %s (%.5f %.5f)" % (param, folder, bl, tr)
 
 def check_opconsat(folder):
     orig = read_ascii(join(tests[folder].testrun, "..", "opconsat", "baseline", "exc.dat"), labels='a b c d', _larch=tests[folder]._larch)
@@ -267,10 +179,15 @@ def check_opconsat(folder):
         failed = True
     assert failed, "columns of exc.dat are not equal to baseline: %s" % ', '.join(which)
 
-def check_clean(folder):
-    assert not isdir(tests[folder].testrun), "clean up %s calculation not successful" % folder
-
 if __name__ == '__main__':
-    t =  Feff8Tests()
-    # t.test_feffrun()
-    t.test_feff()
+    TEST_FOLDERS = ('Copper', 'NiO', 'Zircon', 'ferrocene', 'LCO-para', 'LCO-perp')
+    # TEST_FOLDERS = ('Copper', 'NiO')
+    # TEST_FOLDERS = ALL_FOLDERS
+    for folder in TEST_FOLDERS:
+        t =  Feff8Test(folder)
+        t.test_feffrun()
+        t.test_feff()
+        t.test_terms()
+        t.test_columns_wrapper()
+        t.test_fit()
+        t.test_clean()
