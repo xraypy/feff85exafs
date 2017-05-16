ef9cb20bbc39968d53fa9ee4e2b4aea278814535
Matthew Newville <newville@cars.uchicago.edu>
2017-05-15 22:15:37 -0500

    update Copper script for larch 0.9.34

--- tests/Copper/Copper.py
+++ tests/Copper/Copper.py
@@ -1,9 +1,8 @@
-
-
+import sys
 
 from os.path import realpath, exists, join
 
-from larch import (Group, Parameter, isParameter, param_value, use_plugin_path, isNamedClass)
+from larch import (Group, param_group, Parameter, isParameter, param_value, use_plugin_path, isNamedClass)
 use_plugin_path('io')
 from xdi import read_xdi
 use_plugin_path('xafs')
@@ -27,7 +26,7 @@ def do_fit(self, which):
 
     data = read_xdi(join(self.path, 'Copper.chik'), _larch=self._larch)
 
-    gds = Group(amp    = Parameter(1,     vary=True),
+    gds = param_group(amp    = Parameter(1,     vary=True),
                 enot   = Parameter(1e-7,  vary=True),
                 thetad = Parameter(500,   vary=True),
                 temp   = Parameter(10,    vary=False),
@@ -36,25 +35,27 @@ def do_fit(self, which):
                 _larch=self._larch  )
 
     paths = list()
-    for index in range(1,14):
+    for index in range(1, 14):
         nnnn = realpath(join(folder, "feff%4.4d.dat" % index))
         if not exists(nnnn):
             continue
         #end if
         if index > 1:
-            sigsqr = 'sigma2_debye(temp, thetad)' 
+            sigsqr = 'sigma2_debye(temp, thetad)'
         else:
             sigsqr = 'ss1'
         #end if
-        paths.append(feffpath(nnnn,
-                              s02    = 'amp',
-                              e0     = 'enot',
-                              sigma2 = sigsqr,
-                              deltar = 'alpha*reff', _larch=self._larch))
+
+        paths.append(feffpath(nnnn, s02='amp', e0='enot',
+                              sigma2=sigsqr,
+                              deltar='alpha*reff',
+                              _larch=self._larch))
+
     #end for
 
     trans = feffit_transform(kmin=3, kmax=16, kw=(2,1,3), dk=1, window='hanning', rmin=1.7, rmax=5.1, _larch=self._larch)
     dset  = feffit_dataset(data=data, pathlist=paths, transform=trans, _larch=self._larch)
+
     fit   = feffit(gds, dset, _larch=self._larch)
 
     if self.doplot:
@@ -67,7 +68,7 @@ def do_fit(self, which):
         _plot(dset.data.r,  dset.data.chir_re, label='data', win=2, _larch=self._larch)
         _plot(dset.model.r, dset.model.chir_re, label='fit', win=2, _larch=self._larch)
     #end if
-    
+
     if self.verbose:
         print feffit_report(fit, _larch=self._larch)
     #end if
